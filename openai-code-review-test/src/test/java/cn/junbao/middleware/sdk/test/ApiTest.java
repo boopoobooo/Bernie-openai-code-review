6189d2fa415342b520c38d1b465c7840b309cb24
bernie.huang <Bernie.huang@bamboonetworks.com>
2024-10-24 18:30:01 +0800

    test:test writeLog

--- openai-code-review-test/src/test/java/cn/junbao/middleware/sdk/test/ApiTest.java
+++ openai-code-review-test/src/test/java/cn/junbao/middleware/sdk/test/ApiTest.java
@@ -15,6 +15,6 @@ public class ApiTest {
     public void test(){
         System.out.println(Integer.parseInt("aaa1"));
         System.out.println(Integer.parseInt("aaa2"));
-        System.out.println(Integer.parseInt("aaa6666321"));
+        System.out.println(Integer.parseInt("aaa22222"));
     }
 }
