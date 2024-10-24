df9000e1586841b313557e3744d1fda74e313ba3
bernie.huang <Bernie.huang@bamboonetworks.com>
2024-10-24 16:05:08 +0800

    test:test codeReview-v1

--- openai-code-review-test/src/test/java/cn/junbao/middleware/sdk/test/ApiTest.java
+++ openai-code-review-test/src/test/java/cn/junbao/middleware/sdk/test/ApiTest.java
@@ -13,8 +13,8 @@ public class ApiTest {
 
     @Test
     public void test(){
-        System.out.println(Integer.parseInt("aaa123"));
-        System.out.println(Integer.parseInt("aaa123"));
-        System.out.println(Integer.parseInt("aaa132"));
+        System.out.println(Integer.parseInt("aaa1"));
+        System.out.println(Integer.parseInt("aaa2"));
+        System.out.println(Integer.parseInt("aaa3"));
     }
 }
