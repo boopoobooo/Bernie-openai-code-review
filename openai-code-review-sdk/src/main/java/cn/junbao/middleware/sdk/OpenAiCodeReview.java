c4e9a0c8d074827f30e1909ba4300c15ab777a99
bernie.huang <Bernie.huang@bamboonetworks.com>
2024-10-24 16:04:34 +0800

    feat: codeReview-v1

--- openai-code-review-sdk/src/main/java/cn/junbao/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/cn/junbao/middleware/sdk/OpenAiCodeReview.java
@@ -1,5 +1,6 @@
 package cn.junbao.middleware.sdk;
 
+import cn.junbao.middleware.sdk.model.ChatCompletionRequest;
 import cn.junbao.middleware.sdk.model.ChatCompletionSyncResponse;
 import cn.junbao.middleware.sdk.types.utils.BearerTokenUtils;
 import com.alibaba.fastjson2.JSON;
@@ -8,10 +9,20 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 public class OpenAiCodeReview {
     public static void main(String[] args) throws Exception {
         System.out.println("测试---openaiCodeReview-sdk");
+
+        String diffCode = getGitBranchChangeCode();
+
+        //chatGlM 评审代码
+        String reviewLog = codeReviewGLM(diffCode.toString());
+        System.out.println("评审日志："+reviewLog);
+
+    }
+    public static String getGitBranchChangeCode() throws Exception {
         //1. 代码检出
         ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
         processBuilder.directory(new File("."));
@@ -24,16 +35,11 @@ public class OpenAiCodeReview {
         while ((line = reader.readLine()) != null){
             diffCode.append(line);
         }
+        System.out.println("diffCode = "+  diffCode.toString());
 
         int exitCode = process.waitFor();
         System.out.println("Exited with code: " + exitCode );
-
-        System.out.println("评审代码："+ diffCode);
-
-        //chatGlM 评审代码
-        String reviewLog = codeReviewGLM(diffCode.toString());
-        System.out.println("评审日志："+reviewLog);
-
+        return diffCode.toString();
     }
 
     public static String codeReviewGLM(String diffCode) throws  Exception{
@@ -50,18 +56,16 @@ public class OpenAiCodeReview {
         connection.setRequestProperty("User-Agent","Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
         connection.setDoOutput(true);
 
-        String jsonInpuString = "{"
-                + "\"model\":\"glm-4-plus\","
-                + "\"messages\": ["
-                + "    {"
-                + "        \"role\": \"user\","
-                + "        \"content\": \"你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码为: " + diffCode + "\""
-                + "    }"
-                + "]"
-                + "}";
+        ChatCompletionRequest chatCompletionRequest = new ChatCompletionRequest();
+        chatCompletionRequest.setModel("glm-4-plus");
+        chatCompletionRequest.setMessages(new ArrayList<ChatCompletionRequest.Prompt>(){
+            private static final long serialVersionUID = -7988151926241837899L;
+            {add((new ChatCompletionRequest.Prompt("user","你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码为:")));
+            add(new ChatCompletionRequest.Prompt("user",diffCode));
+            }});
 
         try(OutputStream os = connection.getOutputStream()) {
-            byte[] input = jsonInpuString.getBytes(StandardCharsets.UTF_8);
+            byte[] input = JSON.toJSONString(chatCompletionRequest).getBytes(StandardCharsets.UTF_8);
             os.write(input);
         }
         int responseCode = connection.getResponseCode();
--- openai-code-review-sdk/src/test/java/cn/junbao/middleware/sdk/test/ApiTest.java
+++ openai-code-review-sdk/src/test/java/cn/junbao/middleware/sdk/test/ApiTest.java
@@ -1,5 +1,6 @@
 package cn.junbao.middleware.sdk.test;
 
+import cn.junbao.middleware.sdk.model.ChatCompletionRequest;
 import cn.junbao.middleware.sdk.model.ChatCompletionSyncResponse;
 import cn.junbao.middleware.sdk.types.utils.BearerTokenUtils;
 import com.alibaba.fastjson2.JSON;
@@ -10,6 +11,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 public class ApiTest {
 
@@ -22,8 +24,14 @@ public class ApiTest {
 
     @Test
     public void http_test() throws Exception {
-        String apiKey = "978f055fb6094e16863a5904be5eadcf.q1EoS2jIGub1L86A";
-        String token = BearerTokenUtils.getToken(apiKey);
+        String result = codeReviewGLM("Integer.parseInt(\"abc\")");
+
+        System.out.println(result);
+    }
+
+    public static String codeReviewGLM(String diffCode) throws  Exception{
+        String apiKeySecret = "978f055fb6094e16863a5904be5eadcf.q1EoS2jIGub1L86A";
+        String token = BearerTokenUtils.getToken(apiKeySecret);
 
         System.out.println(token);
         URL url = new URL("https://open.bigmodel.cn/api/paas/v4/chat/completions");
@@ -35,20 +43,16 @@ public class ApiTest {
         connection.setRequestProperty("User-Agent","Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
         connection.setDoOutput(true);
 
-        String code = "1+1";
-
-        String jsonInpuString = "{"
-                + "\"model\":\"glm-4-plus\","
-                + "\"messages\": ["
-                + "    {"
-                + "        \"role\": \"user\","
-                + "        \"content\": \"你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码为: " + code + "\""
-                + "    }"
-                + "]"
-                + "}";
+        ChatCompletionRequest chatCompletionRequest = new ChatCompletionRequest();
+        chatCompletionRequest.setModel("glm-4-plus");
+        chatCompletionRequest.setMessages(new ArrayList<ChatCompletionRequest.Prompt>(){
+            private static final long serialVersionUID = -7988151926241837899L;
+            {add((new ChatCompletionRequest.Prompt("user","你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码为:")));
+                add(new ChatCompletionRequest.Prompt("user",diffCode));
+            }});
 
         try(OutputStream os = connection.getOutputStream()) {
-            byte[] input = jsonInpuString.getBytes(StandardCharsets.UTF_8);
+            byte[] input = JSON.toJSONString(chatCompletionRequest).getBytes(StandardCharsets.UTF_8);
             os.write(input);
         }
         int responseCode = connection.getResponseCode();
@@ -65,6 +69,6 @@ public class ApiTest {
         connection.disconnect();
 
         ChatCompletionSyncResponse response = JSON.parseObject(content.toString(), ChatCompletionSyncResponse.class);
-        System.out.println(response.getChoices().get(0).getMessage().getContent());
+        return response.getChoices().get(0).getMessage().getContent();
     }
 }
