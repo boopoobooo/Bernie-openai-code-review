b3e631dc727243550dd2668a172704b10595749c
junbao <2438229875@qq.com>
2024-10-27 14:41:18 +0800

    fix: codeReview-log url

--- openai-code-review-sdk/src/main/java/cn/junbao/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/cn/junbao/middleware/sdk/OpenAiCodeReview.java
@@ -144,7 +144,7 @@ public class OpenAiCodeReview {
 
     public static String  writeLog(String token , String log) throws  Exception{
         Git git = Git.cloneRepository()
-                .setURI("https://github.com/boopoobooo/Bernie-openAI-codeReview-log.git")
+                .setURI("https://github.com/boopoobooo/Bernie-openai-code-review-log.git")
                 .setDirectory(new File("repo"))
                 .setCredentialsProvider(new UsernamePasswordCredentialsProvider(token, ""))
                 .call();
@@ -164,7 +164,7 @@ public class OpenAiCodeReview {
         git.add().addFilepattern(dateFolderName + "/" + fileName).call();
         git.commit().setMessage("Add new File").call();
         git.push().setCredentialsProvider(new UsernamePasswordCredentialsProvider(token,"")).call();
-        return "https://github.com/boopoobooo/Bernie-openAI-codeReview-log.git/blob/master/" + dateFolderName + "/" + fileName;
+        return "https://github.com/boopoobooo/Bernie-openai-code-review-log.git/blob/master/" + dateFolderName + "/" + fileName;
 
 
     }
